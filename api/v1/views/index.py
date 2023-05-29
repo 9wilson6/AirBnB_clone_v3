52085e9408ee6ba1a341f5a65a1d6e6b49223aff
9wilson6 <gatheruwilson@gmail.com>
2023-05-28 23:56:11 -0700

    Some Stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,4 +10,20 @@ def status_ret():
     """return json"""
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats')
+def stat_ret():
+    """return number of stat"""
+    count_am = storage.count("Amenity")
+    count_ct = storage.count("City")
+    count_pl = storage.count("Place")
+    count_re = storage.count("Review")
+    count_st = storage.count("State")
+    count_us = storage.count("User")
+    return jsonify({"amenities": count_am,
+                    "cities": count_ct,
+                    "places": count_pl,
+                    "reviews": count_re,
+                    "states": count_st,
+                    "users": count_us})
+
 
